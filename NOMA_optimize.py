499af69d9c49ad6e6e1a4c31dbf1e6b77f3214b9
Charlotte <charlotte.muth@student.kit.edu>
2021-04-23 16:58:56 +0200

    small changes

--- NOMA_optimize.py
+++ NOMA_optimize.py
@@ -7,40 +7,61 @@ import matplotlib.pyplot as plt
 rng = np.random.default_rng()
 
 N=[4,8] # number of constellation points for each sender
-iterations= 100
+iterations= 150
 
 
 S1 = np.array([1,-1,1j,-1j])*np.exp(1j*np.pi/4)
+#S1=rng.random((N[0],1)) + 1j*rng.random((N[0],1))
+#S1= np.array([1+1j,1-1j,1+3j,1-3j,-1+1j,-1-1j,-1+3j,-1-3j,3+1j,3-1j,3+3j,3-3j,-3+1j,-3-1j,-3+3j,-3-3j])
+S1=S1/np.max(np.abs(S1))
 
 S2 = 1+rng.random((N[1],1)) + 1j*rng.random((N[1],1))
 S2 = S2/np.max(S2)
 
-C = S1*S2
+C = np.transpose(S1)*S2
 
 plt.scatter(np.real(C), np.imag(C))
 
 
 #optimize S2:
 S2n=S2
+S1n=S1
 S2prog=[]
 
 for iter in range(iterations):
-    for sym in range(N[1]):
+    """ for sym1 in range(N[0]):
         #print(C-S1[0]*S2n[sym])
         #print(sym)
-        s1=rng.integers(0,4) # choose random point from first constellation
-        diff=(C-S1[s1]*S2n[sym])
+        s2=rng.integers(0,N[1]) # choose random point from first constellation
+        diff=(C-S1[sym1]*S2n[s2])
         points=np.nonzero(diff)
 
         # Symbol von dem nächsten Punkt wegschieben
         difn=np.array(diff[points]).flatten()
         mini=np.argmin(np.abs(difn))
         #print(difn[mini])
-        d = difn[mini]/(np.abs(difn[mini])+1)/(iter+1)/S1[s1]
+        d = difn[mini]/(np.abs(difn[mini])+1)/(iter+1)/S2n[s2]
+
+        S1n[sym1]=S1n[sym1]-d
+        S1n=S1n/np.max(np.abs(S1n))
+        C=np.transpose(S1n)*S2n """
+
+    for sym in range(N[1]):
+        #print(C-S1[0]*S2n[sym])
+        #print(sym)
+        s1=rng.integers(0,4) # choose random point from first constellation
+        diff=(C-S1n[s1]*S2n[sym])
+        points=np.nonzero(diff)
+
+        # Symbol von dem nächsten Punkt wegschieben
+        difn=np.array(diff[points]).flatten()
+        mini=np.argmin(np.abs(difn))
+        #print(difn[mini])
+        d = difn[mini]/(np.abs(difn[mini])+1)**5/(iter+1)/S1n[s1]
 
         S2n[sym]=S2n[sym]-d
         S2n=S2n/np.max(np.abs(S2n))
-        C=S1*S2n
+        C=np.transpose(S1n)*S2n
     S2prog.append(S2n)
 
 plt.figure("optimized constellation after "+str(iter)+" iterations")
@@ -52,8 +73,12 @@ for iter in range(iterations):
     if iter==0 or iter==iterations-1:
         plt.scatter(np.real(S2prog[iter]),np.imag(S2prog[iter]),label=iter)
     else:
-        plt.scatter(np.real(S2prog[iter]),np.imag(S2prog[iter]),color='k')
+        if iterations>200:
+            pass
+        else:
+            plt.scatter(np.real(S2prog[iter]),np.imag(S2prog[iter]),color='k')
 plt.legend()
-print(S2n)
+print("S1= " + str(S1n))
+print("S2= " + str(S2n))
 
 plt.show()
