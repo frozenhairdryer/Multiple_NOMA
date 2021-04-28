09e0e52792701c3f8fa90aa78f3396f0d54f514e
Charlotte <charlotte.muth@student.kit.edu>
2021-04-28 22:32:29 +0200

    Begin: Code adaptation from MLOC example Autoencoder

--- nn_def.py
+++ nn_def.py
@@ -1,140 +1,261 @@
+# Code adapted from: 
+# L. Schmalen, M. L. Schmid, and B. Geiger, "Machine Learning and Optimization in Communications - Lecture Examples," available online at http://www.github.org/KIT-CEL/lecture-examples/, 2019
+
+
 import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor, Lambda, Compose
-import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+#from ipywidgets import interactive
+#import ipywidgets as widgets
 rng = np.random.default_rng()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("We are using the following device for learning:",device)
 
 ###############################################################
 ### Parameters ###
-lsym  = 1                      # length of symbol in samples
-h_in  = 1                      # pulseshaping filter
-Csize = [4]                    # size of constellation diagrams [4,4,8] means first sender has 4 bit, second sender 4 and third sender 8 bit
-s_off = [0]                    # sample offset -> model imperfect synchronization
+#lsym  = 1                      # length of symbol in samples
+#h_in  = 1                      # pulseshaping filter
+#Csize = [4]                    # size of constellation diagrams [4,4,8] means first sender has 4 bit, second sender 4 and third sender 8 bit
+#s_off = [0]                    # sample offset -> model imperfect synchronization
 
-train_size = 20                # number of symbols in the training set
-test_size = 10                 # number of symbols in the test data
-batch_size = 5
+#train_size = 20                # number of symbols in the training set
+#test_size = 10                 # number of symbols in the test data
+#batch_size = 5
 
 # channel model is defined inside the nn
+#N_0 = 0.2    # noise power for complex AWGN
 
 ###############################################################
 
-# check integrety of given values
-if np.log2(Csize)==int(np.log2(Csize)):
-    Bit_per_sym = int(np.log2(Csize))   # Bit per symbol for each sender
-else:
-    raise ValueError("Modulation formats need to be of size 2^n. Change Csize")
+# helper function to compute the symbol error rate
+def SER(predictions, labels):
+    return (np.sum(np.argmax(predictions, 1) != labels) / predictions.shape[0])
 
-if np.size(Csize)!=np.size(s_off):
-    raise ValueError("Csize and s_off should be of same size")
+# number of symbols
+M = 4
 
-if train_size/batch_size!=int(train_size/batch_size):
-    raise ValueError("train_size needs to be a multiple of the batch_size")
+EbN0 = 11
 
+# noise standard deviation
+sigma_n = np.sqrt((1/2/np.log2(M)) * 10**(-EbN0/10))
 
-## creation of random message training set
-train_message=rng.integers(0,2,(train_size,Bit_per_sym))
-#print(train_message)
-test_message=rng.integers(0,2,(test_size,Bit_per_sym))
+# validation set. Training examples are generated on the fly
+N_valid = 100000
 
-# Create data loaders.
-train_dataloader = DataLoader(train_message, batch_size=batch_size)
-test_dataloader = DataLoader(test_message, batch_size=batch_size)
+# number of neurons in hidden layers at transmitter
+hidden_neurons_TX_1 = 50
+hidden_neurons_TX_2 = 50
+hidden_neurons_TX_3 = 50
+hidden_neurons_TX_4 = 50
+hidden_neurons_TX = [hidden_neurons_TX_1, hidden_neurons_TX_2, hidden_neurons_TX_3, hidden_neurons_TX_4]
 
-""" # Download training data from open datasets.
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+# number of neurons in hidden layers at receiver
+hidden_neurons_RX_1 = 50
+hidden_neurons_RX_2 = 50
+hidden_neurons_RX_3 = 50
+hidden_neurons_RX_4 = 50
+hidden_neurons_RX = [hidden_neurons_RX_1, hidden_neurons_RX_2, hidden_neurons_RX_3, hidden_neurons_RX_4]
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
+# Generate Validation Data
+y_valid = np.random.randint(M,size=N_valid)
+y_valid_onehot = np.eye(M)[y_valid]
 
-batch_size = 64
+# meshgrid for plotting
+ext_max = 1.8  # assume we normalize the constellation to unit energy than 1.5 should be sufficient in most cases (hopefully)
+mgx,mgy = np.meshgrid(np.linspace(-ext_max,ext_max,400), np.linspace(-ext_max,ext_max,400))
+meshgrid = np.column_stack((np.reshape(mgx,(-1,1)),np.reshape(mgy,(-1,1)))) 
 
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+class Autoencoder(nn.Module):
+    def __init__(self, hidden_neurons_TX, hidden_neurons_RX):
+        super(Autoencoder, self).__init__()
+        # Define Transmitter Layer: Linear function, M input neurons (symbols), 2 output neurons (real and imaginary part)        
+        self.fcT1 = nn.Linear(M,hidden_neurons_TX[0]) 
+        self.fcT2 = nn.Linear(hidden_neurons_TX[0], hidden_neurons_TX[1]) 
+        self.fcT3 = nn.Linear(hidden_neurons_TX[1], hidden_neurons_TX[2]) 
+        self.fcT4 = nn.Linear(hidden_neurons_TX[2], hidden_neurons_TX[3]) 
+        self.fcT5 = nn.Linear(hidden_neurons_TX[3], 2) 
+        
+        # Define Receiver Layer: Linear function, 2 input neurons (real and imaginary part), M output neurons (symbols)
+        self.fcR1 = nn.Linear(2,hidden_neurons_RX[0]) 
+        self.fcR2 = nn.Linear(hidden_neurons_RX[0], hidden_neurons_RX[1]) 
+        self.fcR3 = nn.Linear(hidden_neurons_RX[1], hidden_neurons_RX[2]) 
+        self.fcR4 = nn.Linear(hidden_neurons_RX[2], hidden_neurons_RX[3]) 
+        self.fcR5 = nn.Linear(hidden_neurons_RX[3], M) 
 
-for X, y in test_dataloader:
-    print("Shape of X [N, C, H, W]: ", X.shape)
-    print("Shape of y: ", y.shape, y.dtype)
-    break
-
-# Get cpu or gpu device for training.
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Using {} device".format(device))
-
-# Define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-            nn.ReLU()
-        )
+        # Non-linearity (used in transmitter and receiver)
+        self.activation_function = nn.ELU()      
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        # compute output
+        encoded = self.network_transmitter(x)
+        # compute normalization factor and normalize channel output
+        norm_factor = torch.sqrt(torch.mean(torch.mul(encoded,encoded)) * 2 )                            
+        modulated = encoded / norm_factor    
+        received = self.channel_model(modulated)
+        logits = self.network_receiver(received)
         return logits
+        
+    def network_transmitter(self,batch_labels):
+        out = self.activation_function(self.fcT1(batch_labels))
+        out = self.activation_function(self.fcT2(out))
+        out = self.activation_function(self.fcT3(out))
+        out = self.activation_function(self.fcT4(out))
+        out = self.activation_function(self.fcT5(out))
+        return out
+    
+    def network_receiver(self,inp):
+        out = self.activation_function(self.fcR1(inp))
+        out = self.activation_function(self.fcR2(out))
+        out = self.activation_function(self.fcR3(out))
+        out = self.activation_function(self.fcR4(out))
+        logits = self.activation_function(self.fcR5(out))
+        return logits
+    
+    def channel_model(self,modulated):
+        # just add noise, nothing else
+        received = torch.add(modulated, sigma_n*torch.randn(len(modulated),2).to(device))
+        return received
 
-model = NeuralNetwork().to(device)
-print(model)
+model = Autoencoder(hidden_neurons_TX, hidden_neurons_RX)
+model.to(device)
 
+softmax = nn.Softmax(dim=1)
+
+# Cross Entropy loss
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+# Adam Optimizer
+optimizer = optim.Adam(model.parameters())  
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
 
-        # Backpropagation
-        optimizer.zero_grad()
+# Training parameters
+num_epochs = 30
+batches_per_epoch = 300
+
+# Vary batch size during training
+batch_size_per_epoch = np.linspace(100,10000,num=num_epochs)
+
+validation_SERs = np.zeros(num_epochs)
+validation_received = []
+decision_region_evolution = []
+constellations = []
+
+print('Start Training')
+for epoch in range(num_epochs):
+    
+    batch_labels = torch.empty(int(batch_size_per_epoch[epoch]), device=device)
+
+    for step in range(batches_per_epoch):
+        # Generate training data: In most cases, you have a dataset and do not generate a training dataset during training loop
+        # sample new mini-batch directory on the GPU (if available)        
+        batch_labels.random_(M)
+        batch_labels_onehot = torch.zeros(int(batch_size_per_epoch[epoch]), M, device=device)
+        batch_labels_onehot[range(batch_labels_onehot.shape[0]), batch_labels.long()]=1
+
+        # Propagate (training) data through the net
+        logits = model(batch_labels_onehot)
+
+        # compute loss
+        loss = loss_fn(logits.squeeze(), batch_labels.long())
+
+        # compute gradients
         loss.backward()
+        
+        # Adapt weights
         optimizer.step()
+        
+        # reset gradients
+        optimizer.zero_grad()
+        
+    # compute validation SER
+    out_valid = softmax(model(torch.Tensor(y_valid_onehot).to(device)))
+    validation_SERs[epoch] = SER(out_valid.detach().cpu().numpy().squeeze(), y_valid)
+    print('Validation SER after epoch %d: %f (loss %1.8f)' % (epoch, validation_SERs[epoch], loss.detach().cpu().numpy()))                
+    
+    # calculate and store received validation data
+    encoded = model.network_transmitter(torch.Tensor(y_valid_onehot).to(device))
+    norm_factor = torch.sqrt(torch.mean(torch.mul(encoded,encoded)) * 2 )                            
+    modulated = encoded / norm_factor    
+    received = model.channel_model(modulated)
+    validation_received.append(received.detach().cpu().numpy())
+    
+    # calculate and store constellation
+    encoded = model.network_transmitter(torch.eye(M).to(device))
+    norm_factor = torch.sqrt(torch.mean(torch.mul(encoded,encoded)) * 2 )                            
+    modulated = encoded / norm_factor 
+    constellations.append(modulated.detach().cpu().numpy())
+        
+    # store decision region for generating the animation
+    mesh_prediction = softmax(model.network_receiver(torch.Tensor(meshgrid).to(device)))
+    decision_region_evolution.append(0.195*mesh_prediction.detach().cpu().numpy() + 0.4)
+    
+print('Training finished')
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+cmap = matplotlib.cm.tab20
+base = plt.cm.get_cmap(cmap)
+color_list = base.colors
+new_color_list = [[t/2 + 0.5 for t in color_list[k]] for k in range(len(color_list))]
 
-def test(dataloader, model):
-    size = len(dataloader.dataset)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+# find minimum SER from validation set
+min_SER_iter = np.argmin(validation_SERs)
+ext_max_plot = 1.05*max(max(abs(validation_received[min_SER_iter][:,0])), max(abs(validation_received[min_SER_iter][:,1])))
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model)
-print("Done!") """
+plt.figure(figsize=(6,6))
+font = {'size'   : 14}
+plt.rc('font', **font)
+plt.rc('text', usetex=True)
+    
+plt.plot(validation_SERs,marker='.',linestyle='--')
+plt.scatter(min_SER_iter,validation_SERs[min_SER_iter],marker='o',c='red')
+plt.annotate('Min', (0.95*min_SER_iter,1.4*validation_SERs[min_SER_iter]),c='red')
+plt.xlabel('epoch no.',fontsize=14)
+plt.ylabel('SER',fontsize=14)
+plt.grid(which='both')
+plt.title('SER on Validation Dataset',fontsize=16)
+
+print('Minimum SER obtained: %1.5f (epoch %d out of %d)' % (validation_SERs[min_SER_iter], min_SER_iter, len(validation_SERs)))
+print('The corresponding constellation symbols are:\n', constellations[min_SER_iter])
+
+
+plt.figure(figsize=(19,6))
+font = {'size'   : 14}
+plt.rc('font', **font)
+plt.rc('text', usetex=True)
+    
+plt.subplot(131)
+plt.scatter(constellations[min_SER_iter][:,0], constellations[min_SER_iter][:,1], c=range(M), cmap='tab20',s=50)
+plt.axis('scaled')
+plt.xlabel(r'$\Re\{r\}$',fontsize=14)
+plt.ylabel(r'$\Im\{r\}$',fontsize=14)
+plt.xlim((-ext_max_plot,ext_max_plot))
+plt.ylim((-ext_max_plot,ext_max_plot))
+plt.grid(which='both')
+plt.title('Constellation',fontsize=16)
+
+plt.subplot(132)
+#plt.contourf(mgx,mgy,decision_region_evolution[-1].reshape(mgy.shape).T,cmap='coolwarm',vmin=0.3,vmax=0.7)
+plt.scatter(validation_received[min_SER_iter][:,0], validation_received[min_SER_iter][:,1], c=y_valid, cmap='tab20',s=4)
+plt.axis('scaled')
+plt.xlabel(r'$\Re\{r\}$',fontsize=14)
+plt.ylabel(r'$\Im\{r\}$',fontsize=14)
+plt.xlim((-ext_max_plot,ext_max_plot))
+plt.ylim((-ext_max_plot,ext_max_plot))
+plt.title('Received',fontsize=16)
+
+plt.subplot(133)
+decision_scatter = np.argmax(decision_region_evolution[min_SER_iter], 1)
+plt.scatter(meshgrid[:,0], meshgrid[:,1], c=decision_scatter, cmap=matplotlib.colors.ListedColormap(colors=new_color_list),s=4)
+plt.scatter(validation_received[min_SER_iter][0:4000,0], validation_received[min_SER_iter][0:4000,1], c=y_valid[0:4000], cmap='tab20',s=4)
+plt.axis('scaled')
+plt.xlim((-ext_max_plot,ext_max_plot))
+plt.ylim((-ext_max_plot,ext_max_plot))
+plt.xlabel(r'$\Re\{r\}$',fontsize=14)
+plt.ylabel(r'$\Im\{r\}$',fontsize=14)
+plt.title('Decision regions',fontsize=16)
+
+plt.show()
+#plt.savefig('decision_region_AWGN_AE_EbN0%1.1f_M%d.pdf' % (EbN0,M), bbox_inches='tight')
