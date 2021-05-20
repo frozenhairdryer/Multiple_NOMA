f63d96d49caf58950c8e8fd95fdba840adbd837c
Charlotte <charlotte.muth@student.kit.edu>
2021-05-20 20:04:47 +0200

    Division Canceller

--- nn_enc_dec_divcanc.py
+++ nn_enc_dec_divcanc.py
@@ -23,7 +23,6 @@ print("We are using the following device for learning:",device)
 
 # Training parameters
 num_epochs = 50
-#random_epochs = 30
 batches_per_epoch = 300
 learn_rate =0.005
 
@@ -32,11 +31,10 @@ M = np.array([4,4])
 M_all = np.product(M)
 
 # Definition of noise
-EbN0 = np.array([16,15])
+EbN0 = np.array([16,14])
 
-# loss weights for training: Change if one Encoder is more important:
+# SER weights for training: Change if one SER is more important:
 weight=np.ones(np.size(M))
-# weights are changed during training if one encoder gets stuck
 
 # validation set. Training examples are generated on the fly
 N_valid = 10000
@@ -53,27 +51,23 @@ def SER(predictions, labels):
 # noise standard deviation
 sigma_n = np.sqrt((1/2/np.log2(M)) * 10**(-EbN0/10))
 
-SNR = EbN0+10*np.log10(np.log2(M))
-print("Channel SNR is: "+str(SNR)+" dB")
 
 # Generate Validation Data
 y_valid = rng.integers(0,M,size=(N_valid,np.size(M)))
 
 
 # meshgrid for plotting
-ext_max = 1.5  # assume we normalize the constellation to unit energy than 1.5 should be sufficient in most cases (hopefully)
+ext_max = 2  # assume we normalize the constellation to unit energy than 1.5 should be sufficient in most cases (hopefully)
 mgx,mgy = np.meshgrid(np.linspace(-ext_max,ext_max,400), np.linspace(-ext_max,ext_max,400))
 meshgrid = np.column_stack((np.reshape(mgx,(-1,1)),np.reshape(mgy,(-1,1)))) 
 
 class Encoder(nn.Module):
-    def __init__(self,Mod):
+    def __init__(self,M):
         super(Encoder, self).__init__()
         # Define Transmitter Layer: Linear function, M input neurons (symbols), 2 output neurons (real and imaginary part)        
-        self.fcT1 = nn.Linear(Mod,2*Mod) 
-        self.fcT2 = nn.Linear(2*Mod, 2*Mod)
-        #self.fcT3 = nn.Linear(2*Mod, 2*Mod) 
-        #self.fcT4 = nn.Linear(2*Mod, 2*Mod) 
-        self.fcT5 = nn.Linear(2*Mod, 2) 
+        self.fcT1 = nn.Linear(M,2*M) 
+        self.fcT2 = nn.Linear(2*M, 2*M) 
+        self.fcT5 = nn.Linear(2*M, 2) 
 
         # Non-linearity (used in transmitter and receiver)
         self.activation_function = nn.ELU()      
@@ -92,21 +86,17 @@ class Encoder(nn.Module):
     def network_transmitter(self,batch_labels):
         out = self.activation_function(self.fcT1(batch_labels))
         out = self.activation_function(self.fcT2(out))
-        #out = self.activation_function(self.fcT3(out))
-        #out = self.activation_function(self.fcT4(out))
         out = self.activation_function(self.fcT5(out))
         return out
     
 
 class Decoder(nn.Module):
-    def __init__(self,Mod):
+    def __init__(self,M):
         super(Decoder, self).__init__()
         # Define Receiver Layer: Linear function, 2 input neurons (real and imaginary part), M output neurons (symbols)
-        self.fcR1 = nn.Linear(2,2*Mod) 
-        self.fcR2 = nn.Linear(2*Mod,2*Mod) 
-        self.fcR3 = nn.Linear(2*Mod,2*Mod) 
-        #self.fcR4 = nn.Linear(2*Mod,2*Mod) 
-        self.fcR5 = nn.Linear(2*Mod, Mod) 
+        self.fcR1 = nn.Linear(2,2*M) 
+        self.fcR2 = nn.Linear(2*M,2*M) 
+        self.fcR5 = nn.Linear(2*M, M) 
 
         # Non-linearity (used in transmitter and receiver)
         self.activation_function = nn.ELU()      
@@ -118,48 +108,13 @@ class Decoder(nn.Module):
     
     def network_receiver(self,inp):
         out = self.activation_function(self.fcR1(inp))
-        #out = self.activation_function(self.fcR2(out))
-        out = self.activation_function(self.fcR3(out))
-        #out = self.activation_function(self.fcR4(out))
+        out = self.activation_function(self.fcR2(out))
         logits = self.activation_function(self.fcR5(out))
-        return logits
-
-
-class Canceller(nn.Module):
-    def __init__(self,Mod):
-        super(Canceller, self).__init__()
-        self.fcR1 = nn.Linear(2,Mod) 
-        self.fcR2 = nn.Linear(2,Mod) 
-        self.fcR3 = nn.Linear(Mod,Mod) 
-        self.fcR4 = nn.Linear(Mod,Mod) 
-        self.fcR5 = nn.Linear(Mod, 2) 
-
-        # Non-linearity (used in transmitter and receiver)
-        self.activation_function = nn.ELU()      
-
-    def forward(self, x, decoutput):
-        # compute output
-        logits = self.cancellation(x, decoutput)
-        #norm_factor = torch.max(torch.abs(torch.view_as_complex(logits)).flatten())
-        norm_factor = torch.mean(torch.abs(torch.view_as_complex(logits)).flatten()) # normalize mean amplitude to 1
-        logits = logits/norm_factor
         return logits
     
-    def cancellation(self,inp, decout):
-        #print(self.activation_function(self.fcR1(inp)))
-        #print(self.activation_function(self.fcR2(decout)))
-        out = self.activation_function(self.fcR1(inp))
-        out += self.activation_function(self.fcR2(decout))
-        #out = self.activation_function(self.fcR2(out))
-        #out = self.activation_function(self.fcR3(out))
-        out = self.activation_function(self.fcR4(out))
-        logits = self.activation_function(self.fcR5(out))
-        return logits
-
 # Collect encoders, decoders and optimizer in list
 enc=[]
 dec=[]
-canc = []
 optimizer=[]
 for const in range(np.size(M)):
     
@@ -168,11 +123,8 @@ for const in range(np.size(M)):
     enc[const].to(device)
     # Adam Optimizer
     #optimizer.append([])
-    if const>0:
-            canc.append(Canceller(M_all))
-            optimizer.append(optim.Adam(canc[const-1].parameters(),lr=learn_rate))
-    optimizer.append(optim.Adam(enc[const].parameters(),lr=learn_rate))
-    optimizer.append(optim.Adam(dec[const].parameters(),lr=learn_rate))
+    optimizer.append(optim.Adam(enc[const].parameters(), lr=learn_rate))
+    optimizer.append(optim.Adam(dec[const].parameters(), lr=learn_rate))
 
 
 softmax = nn.Softmax(dim=1)
@@ -188,11 +140,7 @@ batch_size_per_epoch = np.linspace(100,10000,num=num_epochs)
 validation_SERs = np.zeros((np.size(M),num_epochs))
 validation_received = []
 decision_region_evolution = []
-for num in range(np.size(M)):
-    decision_region_evolution.append([])
-
 constellations = []
-cancelled=[]
 
 # constellations only used for plotting
 constellation_base = []
@@ -226,35 +174,19 @@ for epoch in range(num_epochs):
             if num==np.size(M)-1:
                 for dnum in range(np.size(M)):
                     if dnum==0:
-                        decoded[np.size(M)-dnum-1]=dec[np.size(M)-dnum-1](received)
-                        cancelled_train=(canc[dnum](received,enc[np.size(M)-dnum-1](softmax(decoded[np.size(M)-dnum-1]))))
-                    elif dnum==np.size(M)-1:
-                        decoded[np.size(M)-dnum-1]=dec[np.size(M)-dnum-1](cancelled_train)
-                        #decoded.append(dec[np.size(M)-dnum-1](cancelled_train))
+                        decoded[np.size(M)-dnum-1]=(dec[np.size(M)-dnum-1](received))
+                        cancelled = received/enc[np.size(M)-dnum-1](softmax(decoded[np.size(M)-dnum-1]))
                     else:
-                        decoded[np.size(M)-dnum-1]=dec[np.size(M)-dnum-1](cancelled_train)
-                        #decoded.append(dec[np.size(M)-dnum-1](cancelled_train))
-                        cancelled_train=(canc[dnum](received,enc[np.size(M)-dnum-1](softmax(decoded[np.size(M)-dnum-1]))))
-                    
+                        decoded[np.size(M)-dnum-1]=(dec[np.size(M)-dnum-1](cancelled))
+                        cancelled = cancelled/enc[np.size(M)-dnum-1](softmax(decoded[np.size(M)-dnum-1]))
 
         # Calculate Loss
         for num in range(np.size(M)):
-            # if one is stuck we give an incentive to train that encoder (done when evaluating the SERs)
-            # if epoch ==10:
-            #     weight=[2,0.5,0.5]
-            # elif epoch ==15:
-            #     weight=[0.5,2,0.5]
-            # elif epoch ==20:
-            #     weight=[0.5,0.5,2]
-            # else:
-            #     weight=[1,1,1]
-            
             # calculate loss as weighted addition of losses for each enc[x] to dec[x] path
             if num==0:
                 loss = weight[0]*loss_fn(decoded[0], batch_labels[:,0].long())
             else:
                 loss += weight[num]*loss_fn(decoded[num], batch_labels[:,num].long())
-                #loss -= loss_fn(decoded[num], batch_labels[:,num-1].long()) # maximize cross-Entropy of canceller output (here further: decoder output) and already decoded signal
             
         # compute gradients
         
@@ -274,7 +206,6 @@ for epoch in range(num_epochs):
     # compute validation SER
     cvalid = np.zeros(N_valid)
     decoded_valid=[]
-    cancelled.append([])
     for num in range(np.size(M)):
         y_valid_onehot = np.eye(M[num])[y_valid[:,num]]
         decoded_valid.append([])
@@ -293,22 +224,21 @@ for epoch in range(num_epochs):
         if num==np.size(M)-1:
                 for dnum in range(np.size(M)):
                     if dnum==0:
-                        decoded_valid[np.size(M)-dnum-1]=(dec[np.size(M)-dnum-1](channel))
-                        # canceller
-                        cancelled[epoch].append(canc[dnum](channel,enc[np.size(M)-dnum-1](softmax(decoded_valid[np.size(M)-dnum-1]))))
-                    elif dnum==np.size(M)-1:
-                        decoded_valid[np.size(M)-dnum-1]=(dec[np.size(M)-dnum-1](cancelled[epoch][dnum-1]))
+                        decoded_valid[np.size(M)-dnum-1]=dec[np.size(M)-dnum-1](channel)
+                        cancelled = channel/enc[np.size(M)-dnum-1](softmax(decoded_valid[np.size(M)-dnum-1]))
                     else:
-                        decoded_valid[np.size(M)-dnum-1]=(dec[np.size(M)-dnum-1](cancelled[epoch][dnum-1]))
-                        cancelled[epoch].append(canc[dnum](received,enc[np.size(M)-dnum-1](softmax(decoded_valid[np.size(M)-dnum-1]))))
+                        decoded_valid[np.size(M)-dnum-1]=(dec[np.size(M)-dnum-1](cancelled))
+                        cancelled = cancelled/enc[np.size(M)-dnum-1](softmax(decoded_valid[np.size(M)-dnum-1]))
+
 
     
 
     for num in range(np.size(M)):
         out_valid = softmax(decoded_valid[num])
+        #print(out_valid)
         #print(batch_labels[:,num])
         validation_SERs[num][epoch] = SER(out_valid.detach().cpu().numpy().squeeze(), y_valid[:,num])
-        print('Validation SER after epoch %d for encoder %d: %f (loss %1.8f)' % (epoch,num, validation_SERs[num][epoch], loss.detach().cpu().numpy()))
+        print('Validation SER after epoch %d for encoder %d: %f (loss %1.8f)' % (epoch,num, validation_SERs[num][epoch], loss.detach().cpu().numpy()))                
         if validation_SERs[num][epoch]>1/M[num] and epoch>5:
             #Weight is increased, when error probability is higher than symbol probability -> misclassification 
             weight[num] += 1
@@ -318,7 +248,6 @@ for epoch in range(num_epochs):
     if np.sum(validation_SERs[:,epoch])<0.2:
         weight=np.ones(np.size(M))
 
-    
     validation_received.append(channel.detach().cpu().numpy())
     
     # calculate and store base constellations
@@ -342,12 +271,14 @@ for epoch in range(num_epochs):
         
     # store decision region for generating the animation
     for num in range(np.size(M)):
-        #decision_region_evolution.append([])
+        decision_region_evolution.append([])
         if num==0:
-            mesh_prediction = softmax(dec[np.size(M)-num-1](torch.Tensor(meshgrid).to(device)))
+            mesh_prediction = softmax(dec[np.size(M)-num-1].network_receiver(torch.Tensor(meshgrid).to(device)))
+            canc_grid = torch.Tensor(meshgrid).to(device)/enc[np.size(M)-num-1](mesh_prediction)
         else:
-            mesh_prediction = softmax(dec[np.size(M)-num-1](canc[num-1](torch.Tensor(meshgrid).to(device),enc[np.size(M)-num](mesh_prediction))))
-        decision_region_evolution[num].append(0.195*mesh_prediction.detach().cpu().numpy() +0.4)
+            mesh_prediction = softmax(dec[np.size(M)-num-1].network_receiver(canc_grid))
+            canc_grid = canc_grid/enc[np.size(M)-num-1](mesh_prediction)
+        decision_region_evolution[num].append(0.195*mesh_prediction.detach().cpu().numpy() + 0.4)
     
 print('Training finished')
 
@@ -383,7 +314,7 @@ print('Minimum SER obtained: %1.5f (epoch %d out of %d)' % (sum_SERs[min_SER_ite
 print('The corresponding constellation symbols are:\n', constellations[min_SER_iter])
 
 
-plt.figure(figsize=(14,6))
+plt.figure(figsize=(19,6))
 font = {'size'   : 14}
 plt.rc('font', **font)
 plt.rc('text', usetex=True)
@@ -411,30 +342,26 @@ plt.xlim((-ext_max_plot,ext_max_plot))
 plt.ylim((-ext_max_plot,ext_max_plot))
 plt.title('Received',fontsize=16)
 
-plt.figure("Decision regions", figsize=(14,6))
+plt.figure("Decision regions", figsize=(19,6))
 for num in range(np.size(M)):
     plt.subplot(1,np.size(M),num+1)
     decision_scatter = np.argmax(decision_region_evolution[num][min_SER_iter], 1)
     if num==0:
         plt.scatter(meshgrid[:,0], meshgrid[:,1], c=decision_scatter,s=4,cmap=matplotlib.colors.ListedColormap(colors=new_color_list[0:M[num]]))
-        plt.scatter(np.real(val_cmplx[0:4000]), np.imag(val_cmplx[0:4000]), c=cvalid[0:4000], cmap='tab20',s=4)
     else:
-        #vals=cancelled[min_SER_iter][num-1][:,0].detach().numpy()+1j*cancelled[min_SER_iter][num-1][:,1].detach().numpy()
         plt.scatter(meshgrid[:,0], meshgrid[:,1], c=decision_scatter,s=4,cmap=matplotlib.colors.ListedColormap(colors=new_color_list[M[num-1]:M[num-1]+M[num]]))
-        #plt.scatter(np.real(vals[0:4000]), np.imag(vals[0:4000]), c=cvalid[0:4000], cmap='tab20',s=4)
-        plt.scatter(np.real(val_cmplx[0:4000]), np.imag(val_cmplx[0:4000]), c=cvalid[0:4000], cmap='tab20',s=4)
-
     #plt.scatter(validation_received[min_SER_iter][0:4000,0], validation_received[min_SER_iter][0:4000,1], c=y_valid[0:4000], cmap='tab20',s=4)
-        
+    plt.scatter(np.real(val_cmplx[0:4000]), np.imag(val_cmplx[0:4000]), c=cvalid[0:4000], cmap='tab20',s=4)
     plt.axis('scaled')
-    #plt.xlim((-ext_max_plot,ext_max_plot))
-    #plt.ylim((-ext_max_plot,ext_max_plot))
+    plt.xlim((-ext_max_plot,ext_max_plot))
+    plt.ylim((-ext_max_plot,ext_max_plot))
     plt.xlabel(r'$\Re\{r\}$',fontsize=14)
     plt.ylabel(r'$\Im\{r\}$',fontsize=14)
     plt.title('Decision regions for Decoder %d' % num,fontsize=16)
+
 plt.tight_layout()
 
-plt.figure("Base Constellations", figsize=(10,5))
+plt.figure("Base Constellations")
 for num in range(np.size(M)):
     plt.subplot(1,np.size(M),num+1)
     plt.scatter(np.real(constellation_base[num][min_SER_iter].detach().numpy()),np.imag(constellation_base[num][min_SER_iter].detach().numpy()), c=np.arange(M[num]))
@@ -444,6 +371,15 @@ for num in range(np.size(M)):
     plt.ylabel(r'$\Im\{r\}$',fontsize=14)
     plt.grid()
     plt.tight_layout()
+
+#plt.subplot(122)
+#plt.scatter(np.real(constellation_base[1][min_SER_iter].detach().numpy()),np.imag(constellation_base[1][min_SER_iter].detach().numpy()),c=np.arange(M[1]))
+#plt.xlim((-ext_max_plot,ext_max_plot))
+#plt.ylim((-ext_max_plot,ext_max_plot))
+#plt.xlabel(r'$\Re\{r\}$',fontsize=14)
+#plt.ylabel(r'$\Im\{r\}$',fontsize=14)
+#plt.tight_layout()
+
 
 
 plt.show()
