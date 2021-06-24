6f16136d81a422aa733313b235acfbd8dc71c131
Simulation <Simulation@Simulation>
2021-06-24 16:57:11 +0200

    more torch integration

--- canc_compare.py
+++ canc_compare.py
@@ -7,13 +7,13 @@ import pickle
 
 
 ### parameters
-runs = 30
-num_epochs=50
+runs = 10
+num_epochs=60
 
 sigma_n=[0.08,0.08]
 M=[4,4]
-#alph=[1,1/3*np.sqrt(2)]
-alph=[1,1]
+alph=[1,1/3*np.sqrt(2)]
+#alph=[1,1]
 
 params=[runs,num_epochs,sigma_n,M,alph]
 
@@ -21,15 +21,15 @@ params=[runs,num_epochs,sigma_n,M,alph]
 GMI_nocanc=[]
 list_nocanc=[]
 #compare_data.append([])
-for item in range(1):
-    _,en, dec, gmi, ser =Multipl_NOMA(M,sigma_n,train_params=[num_epochs,300,0.005],canc_method='none', modradius=alph, plotting=False)
-    GMI_nocanc.append(gmi)
+for item in range(runs):
+    _,en, dec, gmi, ser, gmi_exact =Multipl_NOMA(M,sigma_n,train_params=[num_epochs,300,0.007],canc_method='none', modradius=alph, plotting=False)
+    GMI_nocanc.append(np.sum(gmi_exact, axis=1))
     list_nocanc.append(ser)
     if item==0:
-        best_impl=[gmi,en, dec, ser]
+        best_impl=[np.sum(gmi_exact, axis=1),en, dec, ser]
         best_achieved='none'
-    elif max(gmi)>max(best_impl[0]):
-        best_impl=[gmi,en, dec, ser]
+    elif max(np.sum(gmi_exact, axis=1))>max(best_impl[0]):
+        best_impl=[np.sum(gmi_exact, axis=1),en, dec, ser]
         best_achieved='none' 
     #exec(open("nn_enc_dec.py").read())
     #list_nocanc.append(validation_SERs)
@@ -44,11 +44,11 @@ list_divcanc=[]
 for item in range(runs):
     try:
         #exec(open("nn_enc_dec_divcanc.py").read())
-        _,en, dec, gmi, ser = Multipl_NOMA(M,sigma_n,train_params=[num_epochs,300,0.001],canc_method='div', modradius=alph, plotting=False)
+        _,en, dec, gmi, ser, gmi_exact = Multipl_NOMA(M,sigma_n,train_params=[num_epochs,300,0.002],canc_method='div', modradius=alph, plotting=False)
         list_divcanc.append(ser)
-        GMI_divcanc.append(gmi)
-        if max(gmi)>max(best_impl[0]):
-            best_impl=[gmi,en, dec, ser]
+        GMI_divcanc.append(np.sum(gmi_exact, axis=1))
+        if max(np.sum(gmi_exact, axis=1))>max(best_impl[0]):
+            best_impl=[np.sum(gmi_exact, axis=1),en, dec, ser]
             best_achieved='div'
     except:
         print("Diverges!")
@@ -63,20 +63,20 @@ list_nncanc=[]
 for item in range(runs):
     #compare_data[2].append(dir())
     #plt.close('all')
-    _,en, dec,canc, gmi, ser = Multipl_NOMA(M,sigma_n,train_params=[num_epochs,300,0.002],canc_method='nn', modradius=alph, plotting=False)
-    GMI_nncanc.append(gmi)
+    _,en, dec,canc, gmi, ser, gmi_exact = Multipl_NOMA(M,sigma_n,train_params=[num_epochs,300,0.002],canc_method='nn', modradius=alph, plotting=False)
+    GMI_nncanc.append(np.sum(gmi_exact, axis=1))
     list_nncanc.append(ser)
     if item==0:
-        best_impl=[gmi,en, dec, canc, ser]
+        best_impl=[np.sum(gmi_exact, axis=1),en, dec, canc, ser]
         best_achieved='nn'
-    elif max(gmi)>max(best_impl[0]):
-        best_impl=[gmi,en, dec, canc, ser]
+    elif max(np.sum(gmi_exact, axis=1))>max(best_impl[0]):
+        best_impl=[np.sum(gmi_exact, axis=1),en, dec, canc, ser]
         best_achieved='nn'
 
 ## save all data if further processing is wanted
-#with open('best_impl_compplot.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-#    pickle.dump([best_impl, best_achieved, params], f)
-with open('cancel_compare_nn_compplot.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+with open('best_impl_gmiexact_compplot.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump([best_impl, best_achieved, params], f)
+with open('cancel_compare_gmiexact_compplot.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
     pickle.dump([list_nocanc,list_divcanc,list_nncanc, best_impl, best_achieved, params], f)
 print("Best implementation achieved with "+best_achieved+" cancellation.")
 
@@ -174,7 +174,7 @@ plt.ylabel('GMI')
 plt.grid()
 plt.ylim(0,4)
 plt.tight_layout()
-plt.savefig('cancell_compare_GMI_Nocanc_modradius.png')
+plt.savefig('cancell_compare_GMI_Nocanc_modradius.pgf')
 
 
 plt.figure("Division Cancellation", figsize=(3.5,2))
@@ -194,7 +194,7 @@ plt.ylabel('GMI')
 plt.ylim(0,4)
 plt.grid()
 plt.tight_layout()
-plt.savefig('cancell_compare_GMI_Divcanc_modradius.png')
+plt.savefig('cancell_compare_GMI_Divcanc_modradius.pgf')
 
 
 plt.figure("NN Cancellation", figsize=(3.5,2))
@@ -212,7 +212,7 @@ plt.ylabel('GMI')
 plt.grid()
 plt.ylim(0,4)
 plt.tight_layout()
-plt.savefig('cancell_compare_GMI_NNcanc_modradius.png')
+plt.savefig('cancell_compare_GMI_NNcanc_modradius.pgf')
 
 plt.figure(figsize=(10,6))
 
@@ -271,21 +271,21 @@ for item in range(runs):
 
 
 
-plt.plot(average_nocanc0, c=color_list[0],linewidth=3, label="Enc"+str(0)+" no cancellation")
-plt.plot(average_nocanc1, c=color_list[2],linewidth=3, label="Enc"+str(1)+" no cancellation")
+plt.plot(average_nocanc0, c=color_list[0],linewidth=2, label="Enc"+str(0)+" no cancellation")
+plt.plot(average_nocanc1, c=color_list[2],linewidth=2, label="Enc"+str(1)+" no cancellation")
 
 plt.fill_between(np.arange(num_epochs), average_nocanc0+var_nocanc0,average_nocanc0-var_nocanc0, color=color_list[0], alpha=0.2)
 plt.fill_between(np.arange(num_epochs), average_nocanc1+var_nocanc1,average_nocanc1-var_nocanc1, color=color_list[2], alpha=0.2)
 
-plt.plot(average_dcanc0, c=color_list[4], linewidth=3, label="Enc"+str(0)+" division cancellation")
-plt.plot(average_dcanc1, c=color_list[6], linewidth=3, label="Enc"+str(1)+" division cancellation")
+plt.plot(average_dcanc0, c=color_list[4], linewidth=2, label="Enc"+str(0)+" division cancellation")
+plt.plot(average_dcanc1, c=color_list[6], linewidth=2, label="Enc"+str(1)+" division cancellation")
 
 plt.fill_between(np.arange(num_epochs), average_dcanc0+var_dcanc0,average_dcanc0-var_dcanc0, color=color_list[4], alpha=0.2)
 plt.fill_between(np.arange(num_epochs), average_dcanc1+var_dcanc1,average_dcanc1-var_dcanc1, color=color_list[6], alpha=0.2)
 
 
-plt.plot(average_nncanc0, c=color_list[8],linewidth=3, label="Enc"+str(0)+" NN cancellation")
-plt.plot(average_nncanc1, c=color_list[10],linewidth=3, label="Enc"+str(1)+" NN cancellation")
+plt.plot(average_nncanc0, c=color_list[8],linewidth=2, label="Enc"+str(0)+" NN cancellation")
+plt.plot(average_nncanc1, c=color_list[10],linewidth=2, label="Enc"+str(1)+" NN cancellation")
 
 plt.fill_between(np.arange(num_epochs), average_nncanc0+var_nncanc0,average_nncanc0-var_nncanc0, color=color_list[8], alpha=0.2)
 plt.fill_between(np.arange(num_epochs), average_nncanc1+var_nncanc1,average_nncanc1-var_nncanc1, color=color_list[10], alpha=0.2)
@@ -296,7 +296,7 @@ plt.yscale('log')
 plt.ylabel('SERs')
 plt.grid()
 plt.tight_layout()
-plt.savefig('cancell_compare_SER_largenet_modradius.png')
+plt.savefig('cancell_compare_SER_modradius.pgf')
 
 plt.show()
 
